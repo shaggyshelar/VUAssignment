65e1dfb13bed604ce3ec53907a9ef31abf491238
Sachin Sarse <Sachin.Sarse>
2018-05-18 18:27:42 +0530

    Integrated UI and css

--- components/photoComponent.js
+++ components/photoComponent.js
@@ -2,19 +2,49 @@ import {connect} from 'react-redux'
 import Link from 'next/link';
 
 function PhotoComponent ({ photos, count }) {
-  let photosList = photos.map((photoDetails, key) => (
-    <div key={key}>
-      <Link href={`/photo?id=${photoDetails.code}`}>
-        <a>{photoDetails.code}</a>
-      </Link>
-    </div>
-  ));
-  return (
-    <div>
-      {photosList}
-    </div>
-  )
-}
+    let photosList = photos.map((photoDetails, key) => (
+      <div key={key} className="imgDiv homeImgBox">
+        <Link href={`/photo?id=${photoDetails.code}&src=${photoDetails.display_src}`}>
+            <img key={key} src={photoDetails.display_src} alt="Image" />
+        </Link>
+        <style jsx>{`
+        .homeImgBox{
+          float: left;
+            width: 30%;
+            padding-left: 15px;
+            padding-right: 15px;
+        }
+        .imgDiv {
+          text-align: center;
+        }
+        .imgDiv img {
+          background: #fff;
+          padding: 5px;
+          margin-bottom: 30px;
+          max-width: 100%;
+          height:auto;
+          display: block;
+          min-height: 250px;
+          box-shadow: 0px 2px 5px rgba(0,0,0,0.1)!important;
+          cursor: pointer;
+        }
+        .imgDiv img:hover{
+          box-shadow: 0 24px 38px 3px rgba(191, 191, 191, 0.14), 0 9px 46px 8px rgba(162, 162, 162, 0.12), 0 11px 15px -7px rgba(21, 21, 21, 0.2)!important;
+        }
+    `}</style>
+      </div>
+    ));
+    return (
+      <div className= "photoDiv">
+        {photosList}
+        <style jsx>{`
+        .photoDiv {
+          margin: 4% 15% 4% 15%;
+        }
+    `}</style>
+      </div>
+    )
+  }
 
 function mapStateToProps (state) {
   const { photos, count } = state
--- pages/photo.js
+++ pages/photo.js
@@ -6,7 +6,7 @@ class Photo extends React.Component {
     static getInitialProps ({ reduxStore, req, query }) {
       const isServer = !!req
       reduxStore.dispatch(getPhotoComments(query.id))
-      return {selectedId: query.id}
+      return {selectedId: query.id, src: query.src}
     }
 
     componentDidMount() {
@@ -17,14 +17,74 @@ class Photo extends React.Component {
     render () {
       let photosList = this.props.photoComments && this.props.photoComments.map((commentDetails, key) => (
         <div key={key}>
-          <h1>{commentDetails.text}</h1>
+          <span className="userColor">{commentDetails.user} </span>
+          <span> {commentDetails.text}</span>
+          <style jsx>{`
+            .userColor{
+              color: #228ae2;
+                font-weight: 500;
+                text-decoration: underline;
+                margin-right: 5px;
+            }
+          `}</style>
         </div>
       ));
       return (
-        <div>
-          <h2>Photo {this.props.selectedId}</h2>
-          {photosList}
-        </div>
+        <div className= "photoDiv mainImgBox ">
+          <div className="imgDiv mainImgDiv noPadding">
+              <img height="" width="100%" src={this.props.src}/> 
+          </div> 
+          <div className="userCommentBox">
+            {photosList}
+          </div>
+          <style jsx>{`
+            .imgDiv {
+              text-align: center;
+              float: left;
+              width: 50%;
+            } 
+            .mainImgDiv img{
+              min-height: 500px;
+              margin-bottom: 0!important;
+            }
+            .mainImgDiv img{
+              box-shadow:none!important;
+              cursor: inherit;
+            }
+            .mainImgDiv img:hover{
+              box-shadow:none!important;
+            }
+            .noPadding{
+              padding: 0!important;                
+            }
+            .photoDiv {
+              margin: 4% 15% 4% 15%;
+            }
+            .mainImgBox{
+              background: #fff;
+              box-shadow: 0px 2px 5px rgba(0,0,0,0.1)!important;
+              min-height: 78vh;
+            }
+            .userCommentBox{
+              line-height: 1.8em;
+              font-size: 15px;
+              font-weight: 400;
+              padding-top: 30px;
+              padding-left: 30px;
+              min-height: 480px;          
+              width: 45%;
+              float: left;
+              background: #fff;
+              border-left: 1px solid #f1f1f1;
+            }
+            .userCommentBox h4{
+                margin-top: 0;
+                margin-bottom: 15px;
+                font-weight: 500;
+                color: #464646;
+            }
+          `}</style>
+        </div>        
       )
     }
   }
